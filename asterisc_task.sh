7ff11be72103e677aaf773d97a4d66e571d55a1e
Alex Molchanov <amol4anov@mail.ru>
2023-03-27 16:12:54 +0300

    asterisk task
    preliminary version

---
+++ asterisc_task.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -e
+#set -x
+
+HOSTDIR="/var/log/test_dir"
+REMOTE_DIR="/var/log/test_dir"
+
+REMOTE="admini@192.168.122.44"
+SSH_KEY_PATH="$HOME/.ssh/id_rsa.pub"
+
+FILES_OLDER_THAN_DAYS="7"
+NFILES=5
+NBYTES_PER_FILE=100
+
+# change MTIME of created files to MODTIMETO.
+CHANGEMTIME=1
+MODTIMETO="8 days ago"
+
+# run in headless mode, dont ask user anything
+HEADLESS=1
+
+# dry run mode. Dont delete any data
+DRY_RUN=0
+
+
+function remove_old_files() {
+    if [ "$DRY_RUN" -eq 1 ]; then
+        ssh "$REMOTE" -i "$SSH_KEY_PATH" "find $REMOTE_DIR/* -mtime +$FILES_OLDER_THAN_DAYS -exec ls -l {} \\;"
+    else
+        ssh "$REMOTE" -i "$SSH_KEY_PATH" "find $REMOTE_DIR/* -mtime +$FILES_OLDER_THAN_DAYS -exec rm {} \\;"
+    fi
+}
+
+function generate_data() {
+    if [ ! -d "$HOSTDIR" ]; then
+
+        if [ "$HEADLESS" -eq 1 ]; then
+            return -1
+        fi
+
+        read -p "$HOSTDIR doesnt exists. Create? (y/n) " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            sudo mkdir -p "$HOSTDIR"
+            sudo chown admini:admini "$HOSTDIR"
+        fi
+
+    fi
+    for (( i=0; i<$(( $NFILES - 1 )); i++ ))
+    do
+        file="$HOSTDIR/file_$i.dat"
+        head -c 100 /dev/random >> "$file"
+
+        if [ $(($i % 2)) -eq 0 ] && [ "$CHANGEMTIME" -eq 1 ]; then
+            touch -d "$MODTIMETO" "$file"
+        fi
+
+    done
+}
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        --remove_old_files )
+            remove_old_files
+            ;;
+        --gen_data )
+            generate_data
+            ;;
+        * )
+            echo "Unknown option $key"
+            exit 1
+            ;;
+    esac
+    shift
+done
